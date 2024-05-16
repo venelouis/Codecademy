5874d9cafa305ad5af0a3ef87523e47dff3d74e1
venelouis <venelouistyago@gmail.com>
2024-05-15 22:16:18 -0300

    ok

--- Learn JavaScript/2. Conditionals/9. RaceDay.js
+++ Learn JavaScript/2. Conditionals/9. RaceDay.js
@@ -9,11 +9,10 @@ They can register early or late.
 Runners are assigned a race number and start time based on their age and registration.
 
 Race number:
-
 Early adults receive a race number at or above 1000.
 All others receive a number below 1000.
-Start time:
 
+Start time:
 Adult registrants run at 9:30 am or 11:00 am.
 Early adults run at 9:30 am.
 Late adults run at 11:00 am.
