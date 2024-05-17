4bad4c1b28c4bb10191da76a233ba287869f0648
venelouis <venelouistyago@gmail.com>
2024-05-16 22:15:42 -0300

    1

---
+++ Learn JavaScript/2. Conditionals/10. quiz.js
@@ -0,0 +1,17 @@
+//1. What will the code block log to the console?
+
+let weather = "spring";
+let clothingChoice = "";
+
+if (weather === "spring") {
+  clothingChoice = "Put on rain boots.";
+} else if (weather === "summer") {
+  clothingChoice = "Make sure to take your sunscreen.";
+} else if (weather === "fall") {
+  clothingChoice = "Wear a light jacket.";
+} else if (weather === 'winter') {
+  clothingChoice = "Wear a heavy coat.";
+} else {
+  console.log('Invalid weather type.');
+};
+console.log(clothingChoice);
